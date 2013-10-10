2173a11a4a8848d8e7048d11acdb8c7f54c926ce
Chris Peoples <chrisofspades@gmail.com>
2013-10-10 09:10:19 -0700

    Enable automatic migrations

--- PickemApp/Migrations/Configuration.cs
+++ PickemApp/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace PickemApp.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(PickemApp.Models.PickemDBContext context)
