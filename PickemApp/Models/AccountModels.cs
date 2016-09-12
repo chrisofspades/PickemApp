9aa9e6e8bf03589d53963779b2c1b1a472fc234b
Chris Peoples <chrisofspades@gmail.com>
2016-09-12 10:49:04 -0700

    Upgrade to .NET 4.5.1

--- PickemApp/Models/AccountModels.cs
+++ PickemApp/Models/AccountModels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Compare = System.ComponentModel.DataAnnotations.CompareAttribute;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Globalization;
